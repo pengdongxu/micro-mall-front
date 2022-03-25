96186d59a0eae8f9d6e6bc6b13fd35b031365894
彭东旭 <pengpdx@163.com>
2022-03-25 15:26:20 +0800

    新增：基础代码新建

---
+++ pages/category/category.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>商品分类列表</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
